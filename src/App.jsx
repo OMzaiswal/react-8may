307e0fff14b3f366b59a0ca5811abec9f5475c1d
OM <omjaiswal2552@gmail.com>
2026-05-12 19:31:27 +0530

    title changed

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ export default function App() {
 
   return (
     <div style={styles.container}>
-      <h1>Todo App1</h1>
+      <h1>Todo Application</h1>
 
       <div style={styles.inputBox}>
         <input
